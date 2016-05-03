3b06b185341ec1f0329e4cf93ba5d37219c12108
YarnSeemannsgarn <janschlenker1990@gmail.com>
2016-05-03 13:15:32 +0200

    Add creation of input fields

--- src/js/json-ld-generator.js
+++ src/js/json-ld-generator.js
@@ -1,3 +1,13 @@
 $( "#sel-thing" ).change(function() {
-    alert( "Changed" );
+    // Remove precreated input fields
+    $(".input-field").remove();
+
+    // What was selected
+    alert($( "#sel-thing option:selected" ).text() + " was selected");
+
+    // Create input fields 
+    // TODO: make generic
+    $("#container").append("<input type=\"text\" class=\"form-control input-field\">");
 });
+
+
