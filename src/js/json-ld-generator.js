11f2f07b09bcd5a4aa06678fe8dd47b30f3759df
YarnSeemannsgarn <janschlenker1990@gmail.com>
2016-05-03 17:32:09 +0200

    Make form creation more generic

--- src/js/json-ld-generator.js
+++ src/js/json-ld-generator.js
@@ -4,16 +4,20 @@ function Event() {
     this.description = null;
 }
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 $( "#sel-thing" ).change(function() {
     // Remove precreated input fields
-    $(".input-field").remove();
+    $(".deleatable").remove();
 
     // What was selected
     // TODO: make generic
     if ($( "#sel-thing option:selected" ).text() == "Event") {
 	var event = new Event();
-	for(var propt in event){
-	    $("#container").append("<input type=\"text\" class=\"form-control input-field\">");
+	for (var attribute in event) {
+	    $("#container").append("<div class=\"form-group deleatable\"><label for=\"" + attribute + "\">" + capitalizeFirstLetter(attribute) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + attribute + "\"></div>");
 	}
     }
 });
