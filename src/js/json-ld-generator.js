b32668859e1009ff5c846fa975d244437e4f702f
Janko Mrkvička <generator@mydevelops.com>
2016-05-04 23:34:48 +0200

    -function for capitalising moved to new file functions.js - added testing options from form - output in console

--- src/js/json-ld-generator.js
+++ src/js/json-ld-generator.js
@@ -1,25 +1,48 @@
-// TODO: Add more fields
-function Event() {
-    this.name = null;
-    this.description = null;
-}
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-$( "#sel-thing" ).change(function() {
-    // Remove precreated input fields
-    $(".deleatable").remove();
-
-    // What was selected
-    // TODO: make generic
-    if ($( "#sel-thing option:selected" ).text() == "Event") {
-	var event = new Event();
-	for (var attribute in event) {
-	    $("#container").append("<div class=\"form-group deleatable\"><label for=\"" + attribute + "\">" + capitalizeFirstLetter(attribute) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + attribute + "\"></div>");
+$("#sel-thing").change(function ()
+{
+	// Remove precreated input fields
+	$(".deleatable").remove();
+	// What was selected
+	// TODO: make generic
+	if ($("#sel-thing option:selected").text() == "Event")
+	{
+		var event = new Event();
+		for (var attribute in event)
+		{
+			$("#container").append("<div class=\"form-group deleatable\"><label for=\"" + attribute + "\">" + capitalizeFirstLetter(attribute) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + attribute + "\"></div>");
+		}
 	}
-    }
 });
 
 
+
+
+/*
+ *
+ *
+ * Loading schema.org objects from jsonld on webpage start
+ *
+ *
+ */
+$.getJSON('/../src/schema/tree.jsonld', {
+	format: "json"
+}, function (json)
+{
+/**
+   creating Schema from received json
+**/
+	var schema = new Schema(json);
+/**
+   getting childrens from schema, childrens are subclases of schema
+**/
+	types = schema.getChildren();
+/**
+   creating list of options based on schema types
+**/
+	Form.createList(types);
+
+
+}).fail(function (jqxhr, textStatus, error)
+{
+	alert("unable to load jsonld!");
+});
