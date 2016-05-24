d9d0edb102a7bcf1d3790da972307b369fd6af62
Janko Mrkvička <generator@mydevelops.com>
2016-05-24 12:06:47 +0200

    dirty code for presentation, but it works

--- src/js/json-ld-generator.js
+++ src/js/json-ld-generator.js
@@ -1,48 +1,60 @@
+var Schema = new Schema();
+
 $("#sel-thing").change(function ()
 {
+	
+	
 	// Remove precreated input fields
 	$(".deleatable").remove();
 	// What was selected
 	// TODO: make generic
-	if ($("#sel-thing option:selected").text() == "Event")
-	{
-		var event = new Event();
-		for (var attribute in event)
-		{
-			$("#container").append("<div class=\"form-group deleatable\"><label for=\"" + attribute + "\">" + capitalizeFirstLetter(attribute) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + attribute + "\"></div>");
-		}
-	}
+	var choosen = $("#sel-thing option:selected").text();
+	var type = Schema.getType(choosen);
+	
+		$.each(type.properties, function(key, value) {
+			//display the key and value pair
+			$("#container").append("<div class=\"form-group deleatable\"><label for=\"" + value + key + "\">" + capitalizeFirstLetter(value) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + value + key + "\"></div>");
+				
+		});
 });
-
-
-
-
-/*
+		
+		/*
  *
  *
  * Loading schema.org objects from jsonld on webpage start
  *
  *
  */
-$.getJSON('/../src/schema/tree.jsonld', {
+ 
+ 
+$.getJSON('/../src/schema/schema.json', {
 	format: "json"
 }, function (json)
 {
 /**
    creating Schema from received json
 **/
-	var schema = new Schema(json);
+	
+	Schema.addJSON(json);
+	//console.log(Schema);
 /**
    getting childrens from schema, childrens are subclases of schema
 **/
-	types = schema.getChildren();
+	//types = schema.getType("Event");
+	//console.log(types);
 /**
    creating list of options based on schema types
 **/
-	Form.createList(types);
+	//Form.createList(types);
 
 
 }).fail(function (jqxhr, textStatus, error)
 {
 	alert("unable to load jsonld!");
 });
+	
+
+
+
+
+
