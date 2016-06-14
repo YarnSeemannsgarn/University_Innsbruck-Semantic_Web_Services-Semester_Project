116f10087fb55fe5fa3a9f931c28a4468edb0b37
YarnSeemannsgarn <janschlenker1990@gmail.com>
2016-06-14 19:22:25 +0200

    Change to two column layout fo properties

--- src/js/json-ld-generator.js
+++ src/js/json-ld-generator.js
@@ -15,9 +15,14 @@ $("#sel-thing").change(function () {
     var choosen = $("#sel-thing option:selected").text();
     var type = Schema.getType(choosen);
     $("#left-column").css("opacity", "0.0");
+    var newRow = true;
     $.each(type.properties, function(key, value) {
+	if (newRow) {
+	    $("#left-column").append("<div class=\"row properties-row deleatable\"></div>");
+	}
+	newRow = !newRow;
 	//display the key and value pair
-	$("#left-column").append("<div class=\"form-group deleatable\"><label for=\"" + value + key + "\">" + capitalizeFirstLetter(value) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + value + key + "\"></div>");
+	$(".properties-row").last().append("<div class=\"col s6 form-group\"><label for=\"" + value + key + "\">" + capitalizeFirstLetter(value) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + value + key + "\"></div>");
     });
     $("#left-column").animate({opacity: 1.0}, 1000);
 });
