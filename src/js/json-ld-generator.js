7fc25c9e15c9cce54b33a55930c8d0e8afee2ce3
YarnSeemannsgarn <janschlenker1990@gmail.com>
2016-06-14 18:33:53 +0200

    Fade in input fields

--- src/js/json-ld-generator.js
+++ src/js/json-ld-generator.js
@@ -7,59 +7,50 @@ $(document).ready(function() {
 // Load schema
 var Schema = new Schema();
 
-$("#sel-thing").change(function ()
-{
-	
-	
-	// Remove precreated input fields
-	$(".deleatable").remove();
-	// What was selected
-	// TODO: make generic
-	var choosen = $("#sel-thing option:selected").text();
-	var type = Schema.getType(choosen);
-	
-		$.each(type.properties, function(key, value) {
-			//display the key and value pair
-			$("#left-column").append("<div class=\"form-group deleatable\"><label for=\"" + value + key + "\">" + capitalizeFirstLetter(value) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + value + key + "\"></div>");
-				
-		});
+$("#sel-thing").change(function () {
+    // Remove precreated input fields
+    $(".deleatable").remove();
+
+    // What was selected
+    var choosen = $("#sel-thing option:selected").text();
+    var type = Schema.getType(choosen);
+    $("#left-column").css("opacity", "0.0");
+    $.each(type.properties, function(key, value) {
+	//display the key and value pair
+	$("#left-column").append("<div class=\"form-group deleatable\"><label for=\"" + value + key + "\">" + capitalizeFirstLetter(value) + ":</label><input type=\"text\" class=\"form-control\" id=\"" + value + key + "\"></div>");
+    });
+    $("#left-column").animate({opacity: 1.0}, 1000);
 });
-		
-		/*
+
+/*
  *
  *
  * Loading schema.org objects from jsonld on webpage start
  *
  *
  */
- 
- 
-$.getJSON('./../src/schema/schema.json', {
-	format: "json"
-}, function (json)
-{
-/**
-   creating Schema from received json
-**/
-	
-	Schema.addJSON(json);
-	//console.log(Schema);
-/**
-   getting childrens from schema, childrens are subclases of schema
-**/
-	//types = schema.getType("Event");
-	//console.log(types);
-/**
-   creating list of options based on schema types
-**/
-	//Form.createList(types);
+$.getJSON('./../src/schema/schema.json', {format: "json"}, function (json) {
+    /**
+       creating Schema from received json
+    **/
+    
+    Schema.addJSON(json);
+    //console.log(Schema);
+    /**
+       getting childrens from schema, childrens are subclases of schema
+    **/
+    //types = schema.getType("Event");
+    //console.log(types);
+    /**
+       creating list of options based on schema types
+    **/
+    //Form.createList(types);
 
 
-}).fail(function (jqxhr, textStatus, error)
-{
-	alert("unable to load jsonld!");
+}).fail(function (jqxhr, textStatus, error) {
+    alert("unable to load jsonld!");
 });
-	
+
 
 
 
